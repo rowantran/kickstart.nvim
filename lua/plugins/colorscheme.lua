189dc3631a071c7c8d9e698138e8760cb68adb06
Rowan-James Tran <rowanjat@amazon.com>
2024-09-24 18:55:31 +0000

    fix colorscheme plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,8 @@ return {
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/vim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Force enable termguicolors (tmux doesn't auto-detect properly)
@@ -13,7 +14,7 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin_macchiato'
+      vim.cmd.colorscheme 'catppuccin-macchiato'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
