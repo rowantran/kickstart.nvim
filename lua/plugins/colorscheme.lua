124d55da332ade9e2790059383a2ffb3496178ed
Rowan-James Tran <rowanjat@amazon.com>
2024-09-25 17:04:46 -0700

    automatically set colorscheme based on system theme
    requires a second script to store a 0/1 flag in a file based on system
    light/dark theme, then sets the colorscheme based on that file

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,36 @@
+local function check_colorscheme()
+  local file = assert(io.open(os.getenv 'HOME' .. '/.desktop_scripts/current_theme', 'r'))
+  local content = file:read '*a'
+  file:close()
+
+  if content:find '^0' then
+    -- dark mode
+    return 'catppuccin-macchiato'
+  else
+    -- light mode
+    return 'catppuccin-latte'
+  end
+end
+
 return {
-    -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Force enable termguicolors (tmux doesn't auto-detect properly)
-      vim.opt.termguicolors = true
+  -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  init = function()
+    -- Force enable termguicolors (tmux doesn't auto-detect properly)
+    vim.opt.termguicolors = true
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme(check_colorscheme())
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
+    -- You can configure highlights by doing something like:
+    vim.cmd.hi 'Comment gui=none'
+  end,
 }
