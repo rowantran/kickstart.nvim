35bbb7cd5ecd6ab92e36ed111c896ad0653310b0
Rowan-James Tran <rowanjat@amazon.com>
2025-01-24 11:57:29 -0800

    nvim: switch dark colorscheme to gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 -- Config
 local currentThemeFile = vim.fn.expand '~/.desktop_scripts/current_theme'
-local dark_colorscheme = 'catppuccin-macchiato'
+local dark_colorscheme = 'gruvbox'
 local light_colorscheme = 'catppuccin-latte'
 -- end config
 
@@ -9,7 +9,9 @@ local function load_colorscheme()
   local content = file:read '*a'
   file:close()
 
-  local colorscheme = (content:find '^0' and dark_colorscheme or light_colorscheme)
+  local is_dark = content:find '^0'
+  vim.o.background = (is_dark and 'dark' or 'light')
+  local colorscheme = (is_dark and dark_colorscheme or light_colorscheme)
   vim.cmd.colorscheme(colorscheme)
 end
 
@@ -28,30 +30,35 @@ watch_file = function()
 end
 
 return {
-  -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  opts = {
-    transparent_background = true,
+  {
+    -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      transparent_background = true,
+    },
+    init = function()
+      -- Force enable termguicolors (tmux doesn't auto-detect properly)
+      vim.opt.termguicolors = true
+
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      load_colorscheme()
+
+      -- Start watching theme file to automatically reload colorscheme once it changes
+      watch_file()
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
   },
-  init = function()
-    -- Force enable termguicolors (tmux doesn't auto-detect properly)
-    vim.opt.termguicolors = true
-
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    load_colorscheme()
-
-    -- Start watching theme file to automatically reload colorscheme once it changes
-    watch_file()
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
+  {
+    "ellisonleao/gruvbox.nvim"
+  }
 }
