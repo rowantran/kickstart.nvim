3bc22fa9aa0dffe23191488550f28cb0b3029b20
Rowan-James Tran <rowanjat@amazon.com>
2025-02-21 22:03:46 +0000

    disable neoscroll on remote machines

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,4 +1,8 @@
 return {
   'karb94/neoscroll.nvim',
   opts = {},
+  enabled = function()
+    local hostname = vim.fn.hostname()
+    return not string.find(hostname, "compute.internal") and not string.find(hostname, "dev-dsk")
+  end
 }
